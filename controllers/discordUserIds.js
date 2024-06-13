37d9654bf758251f9effb18b929bf49278e8acff
ashrafali2000 <ali4282271@gmail.com>
2024-06-13 17:41:33 +0500

    valid user goes to my data base

--- controllers/discordUserIds.js
+++ controllers/discordUserIds.js
@@ -1,12 +1,18 @@
 import { DiscardUserId } from "../models/discordUserIds";
+import { errorHandler } from "../utils/error";
 
 export const createUserWithId = async (req, res, next) => {
   const { userId } = req.body;
-  const newUserWithId = new DiscardUserId({
-    userId,
-  });
-  await newUserWithId.save();
-  res.status(200).send(newUserWithId);
+  const oldUser = await oldUser(userId);
+  if (!oldUser) {
+    const newUserWithId = new DiscardUserId({
+      userId,
+    });
+    await newUserWithId.save();
+    res.status(200).send(newUserWithId);
+  } else {
+    res.send(errorHandler(403, "User exist"));
+  }
 };
 export const getUserWithId = async (req, res, next) => {
   const { userId } = req.body;
--- utils/services.js
+++ utils/services.js
@@ -1,6 +1,11 @@
 import { DiscordBotUser } from "../models/discordBord.js";
+import { DiscardUserId } from "../models/discordUserIds.js";
 
 export const validUser = async (user) => {
   const validUser = await DiscordBotUser.findOne({ userName: user });
+  return validUser;
+};
+export const oldUser = async (id) => {
+  const validUser = await DiscardUserId.findOne({ userId: id });
   return validUser;
 };
